c09047e6fd39b4cb3cc905c9cdc3d108a45dea9d
Lode2 <66075070+Lode2@users.noreply.github.com>
2022-05-06 13:19:46 +0200

    Added clickable move history text.

--- components/newchessboardUI/movelistcanvas.js
+++ components/newchessboardUI/movelistcanvas.js
@@ -1,15 +1,52 @@
+import { borderRadius, padding } from '@mui/system'
+import { useCallback } from 'react'
+
 export default function Movelistcanvas(props) {
     let newList = []
+    const buttonStyle = { all: 'unset', cursor: 'pointer', borderRadius: '3px', paddingLeft: '3px', paddingRight: '3px', marginLeft: '5px' }
 
     if (props.moveList.length !== 0) {
         newList = props.moveList.map((item, index) => {
+            // console.log(index)
             if (index % 2 !== 0) {
-                return " " + item + '\n'
+                return <>
+                    {/* {' '} */}
+                    <button style={buttonStyle} onClick={() => changeMove(index)} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+                        {item}
+                    </button>
+                    {'\n'}
+                </>
             } else {
-                return (index / 2) + 1 + '. ' + item
+                return <>
+                    {(index / 2) + 1 + '.'}
+                    <button style={buttonStyle} onClick={() => changeMove(index)} onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>
+                        {item}
+                    </button>
+                </>
             }
         })
     }
+
+    const changeMove = (moveNr) => {
+        console.log(moveNr + 1)
+    }
+
+    function mouseEnter(event) {
+        event.target.style.background = 'gray';
+        event.target.style.borderRadius = '5px'
+    }
+
+    function mouseLeave(event) {
+        event.target.style.background = 'white';
+    }
+
+    // const changeMove = useCallback((value) => (moveNr) => {
+    //     console.log(value)
+    //     console.log(moveNr)
+    // })
+    // (index) {
+    //     console.log(`go to move ${index}`)
+    // }
 
     return (
         <div style={{ whiteSpace: 'pre-line' }}>{newList}</div>
