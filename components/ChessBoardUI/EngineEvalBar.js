7c12f6e43ee80dfb97b222c05e2b4fc64d9f47d6
Lode2 <66075070+Lode2@users.noreply.github.com>
2022-06-02 13:19:28 +0200

    Added animation to the change of eval bar height.

--- components/ChessBoardUI/EngineEvalBar.js
+++ components/ChessBoardUI/EngineEvalBar.js
@@ -6,8 +6,8 @@ export default function EngineEvalBar(props) {
     const whiteHeight = (currentEval + 10) * 5
     return (
         <div style={{ position: 'relative', height: '100%' }}>
-            <div style={{ height: whiteHeight + '%', backgroundColor: 'white', width: '100%' }}></div>
-            <div style={{ height: 100 - whiteHeight + '%', backgroundColor: 'black', width: '100%' }}></div>
+            <div style={{ height: whiteHeight + '%', backgroundColor: 'white', width: '100%', top: '0', transition: 'height 0.5s ease' }}></div>
+            <div style={{ height: 100 - whiteHeight + '%', backgroundColor: 'black', width: '100%', bottom: '0', transition: 'height 0.5s ease' }}></div>
             <div style={Object.assign({ position: 'absolute', fontSize: '15px', WebkitUserSelect: 'none', MozUserSelect: 'none', msUserSelect: 'none', UserSelect: 'none' }, evalPosition)}>{Math.abs(currentEval) === 10 ? Math.abs(currentEval) : Math.abs(currentEval).toFixed(1)}</div>
         </div>
     )
